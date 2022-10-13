abb736e1fdd21e60cf8d77d8affbc5e04bbebf3a
priyankapaul7562 <108569755+priyankapaul7562@users.noreply.github.com>
2022-10-14 02:29:13 +0530

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "assessment" {
+  description = "A reference (self_link) to the VPC network"
+  value       = demo-instance.assessment.private_ip
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These variables are expected to be passed in by the operator
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "project" {
+  description = "The project ID for the network"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for subnetworks in the network"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "A name prefix used in resource names to ensure uniqueness across a project."
+  type        = string
+}
